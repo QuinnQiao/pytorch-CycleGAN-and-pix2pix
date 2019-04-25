6fc1ebb03190e172e38a97b6a36d80935fe70542
QuinnQiao <1500012919>
2019-04-25 10:10:21 +0800

    fix bug

--- test_cycle.py
+++ test_cycle.py
@@ -19,6 +19,9 @@ if __name__ == '__main__':
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     opt.phase = 'valid'   # for create_dataset, use dataroot/valid*
+    opt.model = 'cycle_gan'
+    opt.dataset_mode = 'unaligned'
+
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
